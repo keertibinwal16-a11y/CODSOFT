dbc283c98404079f90aa6b6f3a324f1d3edc80b4
keertibinwal16-a11y <keertibinwal16@gmail.com>
2026-05-17 13:10:42 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,37 @@
+# Simple Calculator
+
+print("Simple Calculator")
+print("Choose operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+# User input
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Operations
+if choice == '1':
+    result = num1 + num2
+    print("Result:", result)
+
+elif choice == '2':
+    result = num1 - num2
+    print("Result:", result)
+
+elif choice == '3':
+    result = num1 * num2
+    print("Result:", result)
+
+elif choice == '4':
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error! Division by zero is not allowed.")
+
+else:
+    print("Invalid input")
